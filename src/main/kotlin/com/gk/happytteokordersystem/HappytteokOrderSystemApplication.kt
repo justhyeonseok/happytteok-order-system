e778983f0904def27a2c0f1aa9ce8200b3761b63
justhyeonseok <152124370+justhyeonseok@users.noreply.github.com>
2025-09-02 17:42:14 +0900

    Initial commit

---
+++ src/main/kotlin/com/gk/happytteokordersystem/HappytteokOrderSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.gk.happytteokordersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappytteokOrderSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<HappytteokOrderSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/gk/happytteokordersystem/HappytteokOrderSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gk.happytteokordersystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HappytteokOrderSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
