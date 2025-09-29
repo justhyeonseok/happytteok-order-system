62f2373dfe49470008fc714c08c51fe1d8b062a2
jinsso_ol <jinsol982@gmail.com>
2025-09-30 04:30:50 +0900

    Add hasRice property to OrderProductDto and update OrderListRes mapping

--- src/main/kotlin/com/gk/happytteokordersystem/domain/order/dto/OrderListRes.kt
+++ src/main/kotlin/com/gk/happytteokordersystem/domain/order/dto/OrderListRes.kt
@@ -31,7 +31,8 @@ data class OrderListRes(
                     OrderProductDto(
                         productName = it.productType.name,
                         quantity = it.quantity,
-                        unit = it.unit ?: "kg"
+                        unit = it.unit ?: "kg",
+                        hasRice = it.hasRice
                     )
                 }
             )
--- src/main/kotlin/com/gk/happytteokordersystem/domain/order/dto/OrderProductDto.kt
+++ src/main/kotlin/com/gk/happytteokordersystem/domain/order/dto/OrderProductDto.kt
@@ -3,5 +3,6 @@ package com.gk.happytteokordersystem.domain.order.dto
 data class OrderProductDto(
     val productName: String,
     val quantity: Int,
-    val unit: String?
+    val unit: String?,
+    val hasRice: Boolean?
 )
